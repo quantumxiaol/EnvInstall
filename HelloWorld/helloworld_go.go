847633eb26c5e2eadf85077c393ce6c19584dce1
quantumxiaol <liuxiaolong.0425@foxmail.com>
2025-09-01 15:48:47 +0800

    feat: helloworld in go

---
+++ HelloWorld/helloworld_go.go
@@ -0,0 +1,11 @@
+//go:build hello
+
+// go build -o ./output/helloworld ./HelloWorld/helloworld_go.go
+// ./output/hello
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello World")
+}
